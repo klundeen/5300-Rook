e5475f3f1f5ce73c6b95b5534af6bc1d2ad6870c
Zhicong-Zeng <zzzeng888@gmail.com>
2022-04-07 12:18:50 -0700

    Merge sql_to_String.cpp into sql5300.cpp

--- Makefile
+++ Makefile
@@ -6,24 +6,18 @@ INCLUDE_DIR = $(COURSE)/include
 LIB_DIR     = $(COURSE)/lib
 
 # following is a list of all the compiled object files needed to build the sql5300 executable
-OBJS       = sql5300.o sql_to_String.o
-
-# Rule for linking to create the executable
-# Note that this is the default target since it is the first non-generic one in the Makefile: $ make
-sql5300: $(OBJS)
-	g++ -L$(LIB_DIR) -o $@ $(OBJS) -ldb_cxx -lsqlparser
-
-# In addition to the general .cpp to .o rule below, we need to note any header dependencies here
-# idea here is that if any of the included header files changes, we have to recompile
-sql_to_String.o: sql_to_String.h
+OBJS       = sql5300.o
 
 # General rule for compilation
 %.o: %.cpp
 	g++ -I$(INCLUDE_DIR) $(CCFLAGS) -o "$@" "$<"
 
+# Rule for linking to create the executable
+# Note that this is the default target since it is the first non-generic one in the Makefile: $ make
+sql5300: $(OBJS)
+	g++ -L$(LIB_DIR) -o $@ $< -ldb_cxx -lsqlparser
+
 # Rule for removing all non-source files (so they can get rebuilt from scratch)
 # Note that since it is not the first target, you have to invoke it explicitly: $ make clean
 clean:
 	rm -f sql5300 *.o
-
-	
--- sql5300.cpp
+++ sql5300.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include "db_cxx.h"
 #include "SQLParser.h"
-#include "sqlhelper.h"
 
 // CREATE A DIRECTORY IN YOUR HOME DIR ~/cpsc5300/data before running this
 const char *HOME = "cpsc5300/data";
@@ -14,60 +13,114 @@ const unsigned int BLOCK_SZ = 4096;
 using namespace std;
 using namespace hsql;
 
-int main(void) {
-        std::cout << "Have you created a dir: ~/" << HOME  << "? (y/n) " << std::endl;
-        std::string ans;
-        std::cin >> ans;
-        if( ans[0] != 'y')
-                return 1;
-        const char *home = std::getenv("HOME");
-        std::string envdir = std::string(home) + "/" + HOME;
+void printStatementInfo(const SQLStatement *stmt);
+void printSelect(const SelectStatement *stmt);
+void printCreate(const CreateStatement *stmt);
 
-       	DbEnv env(0U);
-        env.set_message_stream(&std::cout);
-        env.set_error_stream(&std::cerr);
-        env.open(envdir.c_str(), DB_CREATE | DB_INIT_MPOOL, 0);
+int main(void)
+{
+    std::cout << "Have you created a dir: ~/" << HOME << "? (y/n) " << std::endl;
+    std::string ans;
+    std::cin >> ans;
+    if (ans[0] != 'y')
+        return 1;
+    const char *home = std::getenv("HOME");
+    std::string envdir = std::string(home) + "/" + HOME;
 
-        Db db(&env, 0);
-        db.set_message_stream(env.get_message_stream());
-        db.set_error_stream(env.get_error_stream());
-        db.set_re_len(BLOCK_SZ); // Set record length to 4K
-        db.open(NULL, EXAMPLE, NULL, DB_RECNO, DB_CREATE | DB_TRUNCATE, 0644); // Erases anything already there
+    DbEnv env(0U);
+    env.set_message_stream(&std::cout);
+    env.set_error_stream(&std::cerr);
+    env.open(envdir.c_str(), DB_CREATE | DB_INIT_MPOOL, 0);
 
-        char block[BLOCK_SZ];
-        Dbt data(block, sizeof(block));
-        int block_number;
-        Dbt key(&block_number, sizeof(block_number));
-        block_number = 1;
-        strcpy(block, "hello!");
-        db.put(NULL, &key, &data, 0);  // write block #1 to the database
+    Db db(&env, 0);
+    db.set_message_stream(env.get_message_stream());
+    db.set_error_stream(env.get_error_stream());
+    db.set_re_len(BLOCK_SZ);                                               // Set record length to 4K
+    db.open(NULL, EXAMPLE, NULL, DB_RECNO, DB_CREATE | DB_TRUNCATE, 0644); // Erases anything already there
 
-        Dbt rdata;
-        db.get(NULL, &key, &rdata, 0); // read block #1 from the database
-        std::cout << "Read (block #" << block_number << "): '" << (char *)rdata.get_data() << "'";
-        std::cout << " (expect 'hello!')" << std::endl;
+    char block[BLOCK_SZ];
+    Dbt data(block, sizeof(block));
+    int block_number;
+    Dbt key(&block_number, sizeof(block_number));
+    block_number = 1;
+    strcpy(block, "hello!");
+    db.put(NULL, &key, &data, 0); // write block #1 to the database
 
-        //SQL entry
-	while(true){
-		cout << "SQL> ";
-		string sql;
-		getline(cin, sql);
-		
-		if(sql == "quit")
-			break;
-		if(sql.length() < 1)
-			continue;
-		
-		//Use SQLParser
-		//SQLParserResult *parser = SQLParser::parseSQLString(sql);
-		hsql::SQLParserResult *parser = hsql::SQLParser::parseSQLString(sql);
-		if(!parser->isValid()){
-			cout << "inValid SQL:"  << endl;
-			continue;
-		}else{
-				
-		}
-		delete parser;
-	}
-	return EXIT_SUCCESS;
+    Dbt rdata;
+    db.get(NULL, &key, &rdata, 0); // read block #1 from the database
+    std::cout << "Read (block #" << block_number << "): '" << (char *)rdata.get_data() << "'";
+    std::cout << " (expect 'hello!')" << std::endl;
+
+    // SQL entry
+    while (true)
+    {
+        cout << "SQL> ";
+        string sql;
+        getline(cin, sql);
+
+        if (sql == "quit")
+            break;
+        if (sql.length() < 1)
+            continue;
+
+        // Use SQLParser
+        // SQLParserResult *parser = SQLParser::parseSQLString(sql);
+        hsql::SQLParserResult *parser = hsql::SQLParser::parseSQLString(sql);
+
+        if (!parser->isValid())
+        {
+            cout << "inValid SQL:" << sql << endl;
+            cout << parser->errorMsg() << endl;
+            continue;
+        }
+        else
+        {
+            for (uint i = 0; i < parser->size(); i++)
+            {
+                printStatementInfo(parser->getStatement(i));
+            }
+        }
+        delete parser;
+    }
+    return EXIT_SUCCESS;
+}
+
+// Sqlhelper function to handle different SQL
+void printStatementInfo(const SQLStatement *stmt)
+{
+    switch (stmt->type())
+    {
+    case kStmtSelect:
+        printSelect((const SelectStatement *)stmt);
+        break;
+    case kStmtCreate:
+        printCreate((const CreateStatement *)stmt);
+        break;
+    /*We don't need these functions now
+    case kStmtInsert:
+        printInsertStatementInfo((const InsertStatement *)stmt, 0);
+        break;
+
+    case kStmtImport:
+        printImportStatementInfo((const ImportStatement *)stmt, 0);
+        break;
+    case kStmtShow:
+        inprint("SHOW", 0);
+    */
+    default:
+        cout << "No implemented" << endl;
+        break;
+    }
+}
+
+// Sqlhelper with printSelectStatementInfo
+void printSelect(const SelectStatement *stmt)
+{
+    cout << "SelectStatement" << endl;
+}
+
+// Sqlhelper with printSelectStatementInfo
+void printCreate(const CreateStatement *stmt)
+{
+    cout << "CreateStatment" << endl;
 }
