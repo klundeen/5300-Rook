02eb12c44e40d1767d20a6d3904cc2de3b934460
Alex Larsen <alarsen@seattleu.edu>
2022-06-06 23:23:58 -0700

    final rev to satisfy Milestone6

--- btree.cpp
+++ btree.cpp
@@ -173,7 +173,7 @@ bool test_btree() {
     row2["b"] = Value(101);
     table.insert(&row1);
     table.insert(&row2);
-    for (int i = 0; i < 100 * 1000; i++) {
+    for (int i = 0; i < 50000; i++) {
         ValueDict row;
         row["a"] = Value(i + 100);
         row["b"] = Value(-i);
